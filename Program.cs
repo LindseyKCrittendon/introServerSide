a4c8312e8594226b539138f48290e80046bd72f0
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-07-14 11:20:38 -0400

    working on exercises in chapter 4

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace chapter4exercises
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            //Planet and Spacechips exercise
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+            // planetList.ForEach (planetList => Console.WriteLine(planetList));
+            // Console.WriteLine(planetList);
+
+            List<string> planetList2 = new List<string>(){"Uranus", "Neptune"};
+            
+
+            planetList.AddRange(planetList2);
+
+            planetList.ForEach(planetList => Console.WriteLine(planetList));
+        }
+    }
+}
